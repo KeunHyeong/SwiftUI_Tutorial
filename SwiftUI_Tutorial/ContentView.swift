9d2dd8d8fc6aecb9d8b5c341f17b5a49936073ff
KeunHyeong <rmsgudxx@gmail.com>
2020-10-25 15:37:33 +0900

    Initial Commit

---
+++ SwiftUI_Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Tutorial
+//
+//  Created by KeunHyeong on 2020/10/25.
+//  Copyright © 2020 KeunHyeong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
